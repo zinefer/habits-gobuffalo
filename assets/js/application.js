bd565644901c291af592bcc8a51f4b27f7c15560
zinefer <zinefer@gmail.com>
2019-11-23 21:02:13 -0700

    Replace Buffalo index with basic Vue.js app

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,25 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
+import Vue from "vue";
+import VueRouter from "router";
+
+import HomeComponent from "./components/home.vue";
+
 $(() => {
+
+  Vue.use(VueRouter);
+
+  const routes = [
+  ];
+
+  const router = new VueRouter({
+    mode: "history",
+    routes
+  });
+
+  const app = new Vue({
+    router
+  }).$mount("#app");
 
 });

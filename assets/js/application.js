191a073930bf7ab9044b572a9bcf14650755e52f
zinefer <zinefer@gmail.com>
2019-11-24 23:21:27 -0700

    Add and integrate bootstrap-vue

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,12 +3,14 @@ require("bootstrap/dist/js/bootstrap.bundle.js");
 
 import Vue from "vue";
 import VueRouter from "router";
+import BootstrapVue from "bootstrap-vue";
 
 import HomeComponent from "./components/home.vue";
 
 $(() => {
 
   Vue.use(VueRouter);
+  Vue.use(BootstrapVue);
 
   const routes = [
     {path: "/", component: HomeComponent}

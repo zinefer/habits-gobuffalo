9c376632fa62191b0856836bcaf65997c895da47
zinefer <zinefer@gmail.com>
2019-11-25 19:52:22 -0700

    Remove Bootstrap

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,16 +1,13 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
-require("bootstrap/dist/js/bootstrap.bundle.js");
 
 import Vue from "vue";
 import VueRouter from "router";
-import BootstrapVue from "bootstrap-vue";
 
 import HomePage from "./pages/home.vue";
 
 $(() => {
 
   Vue.use(VueRouter);
-  Vue.use(BootstrapVue);
 
   const routes = [
     {path: "/", component: HomePage}
